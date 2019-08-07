477f571d1b353e6bbe8eb731df3f2527a2328015
dssj888@163.com <>
2019-08-07 19:12:36 +0800

    更新支付密码界面

--- xasq/User/Controllers/PayAndLoginPasswordViewController.m
+++ xasq/User/Controllers/PayAndLoginPasswordViewController.m
@@ -13,10 +13,12 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLB;
 @property (weak, nonatomic) IBOutlet UIView *oldView;  // 旧密码背景View
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *lineViewTop;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *affirmBtnTop;
 
 @property (weak, nonatomic) IBOutlet UITextField *oldPasswordTF; // 旧密码
 @property (weak, nonatomic) IBOutlet UITextField *passwordTF;  // 新密码
 @property (weak, nonatomic) IBOutlet UITextField *affirmPasswordTF; // 确认密码
+@property (weak, nonatomic) IBOutlet UIView *passwordView;
 
 @property (weak, nonatomic) IBOutlet UIButton *affirmBtn; // 确认按钮
 @end
@@ -27,11 +29,14 @@
     [super viewDidLoad];
         if (_type == 0) {
         self.title = @"修改密码";
+            _affirmBtnTop.constant = 50;
     } else {
         self.title = @"设置支付密码";
         _titleLB.text = @"支付密码为6位数字密码";
         _oldView.hidden = YES;
+        _passwordView.hidden = NO;
         _lineViewTop.constant = 15;
+        _affirmBtnTop.constant = 180;
         _passwordTF.placeholder = @"请输入支付密码";
         _affirmPasswordTF.placeholder = @"请确认支付密码";
         [_affirmBtn setTitle:@"确认" forState:(UIControlStateNormal)];
