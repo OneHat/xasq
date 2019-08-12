614438a25cc5fe63418c03769668cf6c4dbb8c03
丁鹏飞 <1005079849@qq.com>
2019-08-12 10:12:22 +0800

    update requestapi

--- xasq/Tool/NetworkManager/RequestApi.h
+++ xasq/Tool/NetworkManager/RequestApi.h
@@ -30,6 +30,52 @@ FOUNDATION_EXPORT NSString *const UserInviteSecond;
 //申请实名认证
 FOUNDATION_EXPORT NSString *const UserIdentityApply;
 
+//查看个人总帐单 POST
+FOUNDATION_EXPORT NSString *const ApiAccountSelectAccounntBill;
+//帐号开户 POST
+FOUNDATION_EXPORT NSString *const ApiAccountOpenAccount;
+//查看金融工具币种 POST
+FOUNDATION_EXPORT NSString *const ApiAccountSelectCurrency;
+
+
+//获取系统版本信息 Get
+FOUNDATION_EXPORT NSString *const OperationSystemVersion;
+//获取banner信息 Get
+FOUNDATION_EXPORT NSString *const OperationBanner;
+
+
+//获取推荐任务 GET
+FOUNDATION_EXPORT NSString *const CommunityTaskRecommend;
+//获取用户好友社区资产动态（领币，偷币，奖励…）GetGet
+FOUNDATION_EXPORT NSString *const CommunityCapitalWaterDynamics;
+//获取高分任务 GET
+FOUNDATION_EXPORT NSString *const CommunityTaskPower;
+//获取用户社区资产账户流水 Get
+FOUNDATION_EXPORT NSString *const CommunityCapitalWater;
+//获取用户算力奖励 Get
+FOUNDATION_EXPORT NSString *const CommunitySendPower;
+//获取我的任务 Get
+FOUNDATION_EXPORT NSString *const CommunityTask;
+//用户算力记录 Get
+FOUNDATION_EXPORT NSString *const CommunityPowerRecord;
+//用户收取/领取的录 POST
+FOUNDATION_EXPORT NSString *const CommunityAreaTakeRecord;
+//用户收币 POST
+FOUNDATION_EXPORT NSString *const CommunityAreaTakeCurrency;
+//获取单个矿区信息 GET
+FOUNDATION_EXPORT NSString *const CommunityArea;
+//获取所有矿区信息 GET
+FOUNDATION_EXPORT NSString *const CommunityAreaList;
+//用户社区账户资产统计 POST
+FOUNDATION_EXPORT NSString *const CommunityCapitalStatistics;
+//新建社区资产账户 POST
+FOUNDATION_EXPORT NSString *const CommunityUserSubAccount;
+//用户偷币 POST
+FOUNDATION_EXPORT NSString *const CommunityAreaStealCurrency;
+//获取用户步行奖励 POST
+FOUNDATION_EXPORT NSString *const CommunitySendWalk;
+
+
 @interface RequestApi : NSObject
 
 @end
--- xasq/Tool/NetworkManager/RequestApi.m
+++ xasq/Tool/NetworkManager/RequestApi.m
@@ -19,6 +19,31 @@ NSString *const UserInviteSendmessage = @"user/invite/sendmessage";
 NSString *const UserInviteSecond = @"user/invite/Second";
 NSString *const UserIdentityApply = @"user/identity/apply";
 
+NSString *const ApiAccountSelectAccounntBill = @"api/account/selectAccounntBill";
+NSString *const ApiAccountOpenAccount = @"api/account/openAccount";
+NSString *const ApiAccountSelectCurrency = @"api/account/selectCurrency";
+
+NSString *const OperationSystemVersion = @"operation/sys/version";
+NSString *const OperationBanner = @"operation/banner";
+
+
+NSString *const CommunityTaskRecommend = @"community/task/recommend";
+NSString *const CommunityCapitalWaterDynamics = @"community/capital/water/dynamics";
+NSString *const CommunityTaskPower = @"community/task/power";
+NSString *const CommunityCapitalWater = @"community/capital/water";
+NSString *const CommunitySendPower = @"community/send/power";
+NSString *const CommunityTask = @"community/task";
+NSString *const CommunityPowerRecord = @"community/power/Record";
+NSString *const CommunityAreaTakeRecord = @"community/area/take/record";
+NSString *const CommunityAreaTakeCurrency = @"community/area/takeCurrency";
+NSString *const communityArea = @"community/area";
+NSString *const CommunityAreaList = @"community/area/list";
+NSString *const CommunityCapitalStatistics = @"community/capital/statistics";
+NSString *const CommunityUserSubAccount = @"community/user/sub/account";
+NSString *const CommunityAreaStealCurrency = @"community/area/stealCurrency";
+NSString *const CommunitySendWalk = @"community/send/walk";
+
+
 @implementation RequestApi
 
 @end
