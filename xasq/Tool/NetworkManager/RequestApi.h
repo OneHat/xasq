c5814465300e422b95e241f6a931e98064f6a6d8
dssj888@163.com <>
2019-08-12 10:07:53 +0800

    新增接口地址

--- xasq/Tool/NetworkManager/RequestApi.h
+++ xasq/Tool/NetworkManager/RequestApi.h
@@ -10,25 +10,46 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-//注册
+/// 注册 POST
 FOUNDATION_EXPORT NSString *const UserRegister;
-//手机登录
+/// 手机登录 POST
 FOUNDATION_EXPORT NSString *const UserLoginMobile;
-//邮箱登录
+/// 邮箱登录 POST
 FOUNDATION_EXPORT NSString *const UserLoginEmail;
 
-//用户基本信息
+/// 用户基本信息 GET
 FOUNDATION_EXPORT NSString *const UserInfo;
-//谷歌绑定
+/// 谷歌绑定 POST
 FOUNDATION_EXPORT NSString *const UserGoogleBind;
-//查询用户通讯录
+/// 邮箱绑定 POST
+FOUNDATION_EXPORT NSString *const UserEmailBind;
+/// 查询用户通讯录 GET
 FOUNDATION_EXPORT NSString *const UserInviteAddresslist;
-//发送邀请短信
+/// 发送邀请短信 POST
 FOUNDATION_EXPORT NSString *const UserInviteSendmessage;
-//查询二级邀请算力奖励记录
+/// 查询二级邀请算力奖励记录
 FOUNDATION_EXPORT NSString *const UserInviteSecond;
-//申请实名认证
+/// 申请实名认证 POST
 FOUNDATION_EXPORT NSString *const UserIdentityApply;
+/// 手机绑定接口 POST
+FOUNDATION_EXPORT NSString *const UserMobileBind;
+/// 查询所有邀请算力奖励记录 GET
+FOUNDATION_EXPORT NSString *const UserInviteAll;
+/// 查询一级邀请算力奖励记录 GET
+FOUNDATION_EXPORT NSString *const UserInviteFirst;
+/// 获取我的界面信息 POST
+FOUNDATION_EXPORT NSString *const HomePageInfo;
+/// 邀请人绑定接口 POST
+FOUNDATION_EXPORT NSString *const UserInviteBind;
+/// 获取邀请码参数接口 GET
+FOUNDATION_EXPORT NSString *const UserInviteQrcode;
+/// 用户签到接口 POST
+FOUNDATION_EXPORT NSString *const UserSignIn;
+/// 修改密码接口 POST
+FOUNDATION_EXPORT NSString *const UserPwdLoginModify;
+/// 查询签到信息 GET
+FOUNDATION_EXPORT NSString *const UserSignInfo;
+
 
 @interface RequestApi : NSObject
 
--- xasq/Tool/NetworkManager/RequestApi.m
+++ xasq/Tool/NetworkManager/RequestApi.m
@@ -8,16 +8,26 @@
 
 #import "RequestApi.h"
 
-NSString *const UserRegister = @"user/register/standard";
+NSString *const UserRegister = @"user/register";
 NSString *const UserLoginMobile = @"user/login/mobile";
 NSString *const UserLoginEmail = @"user/login/email";
 
 NSString *const UserInfo = @"user/info";
 NSString *const UserGoogleBind = @"user/google/bind";
+NSString *const UserEmailBind = @"user/email/bind";
 NSString *const UserInviteAddresslist = @"user/invite/addresslist";
 NSString *const UserInviteSendmessage = @"user/invite/sendmessage";
 NSString *const UserInviteSecond = @"user/invite/Second";
 NSString *const UserIdentityApply = @"user/identity/apply";
+NSString *const UserMobileBind = @"user/mobile/bind";
+NSString *const UserInviteAll = @"user/invite/all";
+NSString *const UserInviteFirst = @"user/invite/first";
+NSString *const HomePageInfo = @"home/page/info";
+NSString *const UserInviteBind = @"user/invite/bind";
+NSString *const UserInviteQrcode = @"user/invite/qrcode";
+NSString *const UserSignIn = @"user/sign/in";
+NSString *const UserPwdLoginModify = @"user/pwd/login/modify";
+NSString *const UserSignInfo = @"user/sign/info";
 
 @implementation RequestApi
 
