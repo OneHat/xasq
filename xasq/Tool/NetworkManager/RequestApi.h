1fb6c7f558554473afa34f4ed555d8d29fa57c97
丁鹏飞 <1005079849@qq.com>
2019-08-12 09:42:49 +0800

    接口API

--- xasq/Tool/NetworkManager/RequestApi.h
+++ xasq/Tool/NetworkManager/RequestApi.h
@@ -10,8 +10,25 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-///注册接口
+//注册
 FOUNDATION_EXPORT NSString *const UserRegister;
+//手机登录
+FOUNDATION_EXPORT NSString *const UserLoginMobile;
+//邮箱登录
+FOUNDATION_EXPORT NSString *const UserLoginEmail;
+
+//用户基本信息
+FOUNDATION_EXPORT NSString *const UserInfo;
+//谷歌绑定
+FOUNDATION_EXPORT NSString *const UserGoogleBind;
+//查询用户通讯录
+FOUNDATION_EXPORT NSString *const UserInviteAddresslist;
+//发送邀请短信
+FOUNDATION_EXPORT NSString *const UserInviteSendmessage;
+//查询二级邀请算力奖励记录
+FOUNDATION_EXPORT NSString *const UserInviteSecond;
+//申请实名认证
+FOUNDATION_EXPORT NSString *const UserIdentityApply;
 
 @interface RequestApi : NSObject
 
--- xasq/Tool/NetworkManager/RequestApi.m
+++ xasq/Tool/NetworkManager/RequestApi.m
@@ -8,7 +8,16 @@
 
 #import "RequestApi.h"
 
-NSString *const UserRegister = @"";
+NSString *const UserRegister = @"user/register/standard";
+NSString *const UserLoginMobile = @"user/login/mobile";
+NSString *const UserLoginEmail = @"user/login/email";
+
+NSString *const UserInfo = @"user/info";
+NSString *const UserGoogleBind = @"user/google/bind";
+NSString *const UserInviteAddresslist = @"user/invite/addresslist";
+NSString *const UserInviteSendmessage = @"user/invite/sendmessage";
+NSString *const UserInviteSecond = @"user/invite/Second";
+NSString *const UserIdentityApply = @"user/identity/apply";
 
 @implementation RequestApi
 
