aaff1021bf317b98c39fdc401f8647722f97d996
dssj888@163.com <>
2019-08-14 19:40:12 +0800

    新增修改手机号界面

---
+++ xasq/User/Controllers/ChangePhoneAndEmailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ChangePhoneAndEmailViewController.h
+//  xasq
+//
+//  Created by dssj888@163.com on 2019/8/14.
+//  Copyright © 2019 dssj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChangePhoneAndEmailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ xasq/User/Controllers/ChangePhoneAndEmailViewController.m
@@ -0,0 +1,32 @@
+//
+//  ChangePhoneAndEmailViewController.m
+//  xasq
+//
+//  Created by dssj888@163.com on 2019/8/14.
+//  Copyright © 2019 dssj. All rights reserved.
+//
+
+#import "ChangePhoneAndEmailViewController.h"
+
+@interface ChangePhoneAndEmailViewController ()
+
+@end
+
+@implementation ChangePhoneAndEmailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
